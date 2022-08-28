c76dd1715b7cce3ab8a1bdda7eee8c9d2a0875ac
ahmadmsvh <ahmadmsvh@gmail.com>
2022-08-28 13:27:13 +0430

    2022 Aug 28

---
+++ learn/J01.java
@@ -0,0 +1,9 @@
+package learn;
+
+public class J01 {
+    public static void main(String[] args) {
+        byte myNum = 120;
+        System.out.println(Math.random());  
+        System.out.println((int) (Math.random()*100));  
+      }
+}
---
+++ learn/J02.java
@@ -0,0 +1,10 @@
+package learn;
+
+public class J02 {
+    public static void main(String[] args){
+        int input = 90;
+        boolean input_greater_than_hundred = (input > 100)? true:false;
+        System.out.println(input_greater_than_hundred);
+    }
+    
+}
